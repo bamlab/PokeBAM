c235ae87e6a7b7c0246958872d45ad04d32a7623
Guillaume Diallo-Mulliez <guillaumedm@bamlab.fr>
2018-06-10 16:58:18 +0200

    Trigger bullet stream on weapon loaded

---
+++ src/components/BulletStream.js
@@ -0,0 +1,68 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import { StyleSheet } from 'react-native';
+
+import { ViroSphere, ViroMaterials } from 'react-viro';
+
+const BULLET_POSITION_FACTOR = 0.3;
+
+export default class BulletStream extends Component {
+  constructor() {
+    super();
+    this.state = {
+      position: [0, 0, -1],
+      alive: true,
+    };
+  }
+  static getDerivedStateFromProps(props) {
+    return {
+      position: [
+        props.position[0] - props.up[0] * BULLET_POSITION_FACTOR,
+        props.position[1] - props.up[1] * BULLET_POSITION_FACTOR,
+        props.position[2] - props.up[2] * BULLET_POSITION_FACTOR,
+      ],
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        ...BulletStream.getDerivedStateFromProps(this.props),
+        alive: !this.state.alive,
+      });
+    }, 300);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    if (!this.state.alive) return null;
+    return (
+      <ViroSphere
+        heightSegmentCount={20}
+        widthSegmentCount={20}
+        radius={2}
+        position={this.state.position}
+        scale={[0.025, 0.025, 0.025]}
+        physicsBody={{
+          type: 'Dynamic',
+          mass: 1,
+          useGravity: false,
+          force: {
+            value: this.props.force,
+          },
+        }}
+        materials={['spherematerial']}
+      />
+    );
+  }
+}
+
+ViroMaterials.createMaterials({
+  spherematerial: {
+    diffuseColor: '#FF0',
+  },
+});
+
+module.exports = BulletStream;
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-import React, { Component } from "react";
-import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import React, { Component } from 'react';
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
 
-const pokeballImage = require("PokeBAM/src/assets/pokeball_button.png");
-const weaponImage = require("PokeBAM/src/assets/weapon_button.png");
+const pokeballImage = require('PokeBAM/src/assets/pokeball_button.png');
+const weaponImage = require('PokeBAM/src/assets/weapon_button.png');
 
-import WeaponEnabledContext from "PokeBAM/src/WeaponEnabledContext";
+import WeaponEnabledContext from 'PokeBAM/src/WeaponEnabledContext';
 
 export default class Toolbar extends Component {
   constructor(props) {
@@ -15,14 +15,15 @@ export default class Toolbar extends Component {
   render() {
     return (
       <WeaponEnabledContext.Consumer>
-        {({ toggleWeaponEnabled, weaponEnabled }) => {
+        {({ toggleWeaponEnabled, weaponEnabled, onWeaponLoaded }) => {
           const image = weaponEnabled ? pokeballImage : weaponImage;
           return (
             <View>
               {weaponEnabled ? (
                 <Image
                   style={styles.gun}
-                  source={require("PokeBAM/src/assets/weapon.png")}
+                  source={require('PokeBAM/src/assets/weapon.png')}
+                  onLoadEnd={onWeaponLoaded}
                 />
               ) : null}
               <TouchableOpacity
@@ -41,26 +42,26 @@ export default class Toolbar extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 0,
     right: 0,
     width: 100,
-    height: 100
+    height: 100,
   },
   gun: {
-    position: "absolute",
+    position: 'absolute',
     right: 110,
     bottom: 0,
     width: 200,
-    height: 150
+    height: 150,
   },
   image: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 20,
     right: 20,
     width: 80,
-    height: 80
-  }
+    height: 80,
+  },
 });
 
 module.exports = Toolbar;
